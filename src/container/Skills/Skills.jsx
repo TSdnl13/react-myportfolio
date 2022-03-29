6fe79c37a132e64cedcc1756ae670815e1306c43
TSdnl13 <daniel.int@outlook.com>
2022-03-29 12:20:50 -0500

    some fixes

--- src/container/Skills/Skills.jsx
+++ src/container/Skills/Skills.jsx
@@ -26,7 +26,7 @@ const Skills = () => {
          <div className='app__skills-list'>
             {skills.map((skill, index) => (
                <motion.div
-                  initial={{x: (index % 2 == 0 ? "-50%":"50%")}}
+                  initial={{x: (index % 2 === 0 ? "-50%":"50%")}}
                   whileInView={{opacity: [0, 1], x: 0 }}
                   transition={{duration: 0.8 }}
                   className='app__skills-item'
